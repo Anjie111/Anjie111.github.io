bea43ff92e1113ff5fcf60f55df08f1077d94768
1264157114@qq.com <1264157114@qq.com>
2024-07-15 13:46:18 +0800

    Site updated: 2024-07-15 13:46:18

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1721021922770";
+const VERSION = "1721022374828";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
